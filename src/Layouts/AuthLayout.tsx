adae6a713e30cc621e428912c5ccf1b39e23c7a7
afiqismail01 <119737047+afiqismail01@users.noreply.github.com>
2025-11-17 00:25:31 +0800

    remove customizer

--- src/Layouts/AuthLayout.tsx
+++ src/Layouts/AuthLayout.tsx
@@ -1,7 +1,4 @@
-import { useEffect, Suspense, ReactNode } from 'react'
-import { ThemeSettings, useThemeContext } from '../common/context'
-import { changeHTMLAttribute } from '../utils'
-import { Button, Stack } from 'react-bootstrap'
+import { Suspense, ReactNode } from 'react'
 import { PreloaderFull } from '@/components/Misc/Preloader'
 
 interface AuthLayoutProps {
@@ -9,34 +6,12 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  const { settings, updateSettings } = useThemeContext()
-
-  const handleCustomizer = () => {
-    updateSettings({ customizer: ThemeSettings.customizer.show })
-  }
-
-  useEffect(() => {
-    changeHTMLAttribute('data-color-scheme', settings.color)
-    changeHTMLAttribute('data-bs-theme', settings.theme)
-    changeHTMLAttribute('data-theme-font', settings.font)
-    changeHTMLAttribute('data-content-skin', settings.layout.contentSkin)
-  }, [settings])
 
   return (
     <Suspense fallback={<PreloaderFull />}>
       <Suspense fallback={<div />}>
         <div className="wrapper">{children}</div>
       </Suspense>
-
-      <Stack className="position-fixed z-1" style={{ right: '0', bottom: '50%' }}>
-        <Button
-          onClick={handleCustomizer}
-          variant="primary"
-          className="btn-lg btn-icon rounded-0 rounded-start-3"
-        >
-          <i className="fi fi-rr-settings fs-18" />
-        </Button>
-      </Stack>
 
       {/* <Stack
         className="support-livechat-btn position-fixed z-1"
